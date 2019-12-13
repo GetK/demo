c8af47d9b1601817b7dae3b6d6eb0fb60fb44988
黄奖 <1593142519@qq.com>
2019-12-13 16:48:26 +0800

    调试

---
+++ Assets/Movess.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movess : MonoBehaviour
+{
+    Transform cam;
+    Rigidbody rigidbody;
+    Vector3 fown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 forward = (transform.position - cam.position);
+        forward.y = 0;
+        forward = forward.normalized;
+
+        Vector3 right = Quaternion.Euler(0, 90, 0) * forward;
+        fown = forward*v + h*right;
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(fown * 20);
+    }
+}
---
+++ Assets/Script/Move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float higiht;
+    public float radius;
+    int dirIndex = 0;
+
+    public Transform Targetpos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+    void FixedUpdate()
+    {
+        Vector3[] pose = new Vector3[]
+        {
+            new Vector3(0,0,-radius),
+            new Vector3 (radius,0,0),
+            new Vector3 (0,0,radius),
+            new Vector3(-radius,0,0)
+        };
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+                dirIndex++;
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+                dirIndex--;
+        }
+        if (dirIndex > 3) dirIndex = 0;
+        if (dirIndex < 0) dirIndex = 3;
+
+        Vector3 offset = transform.position - Targetpos.position;
+        offset.y = 0;
+        Vector3 temp = pose[dirIndex];
+        offset = Vector3.Slerp(offset, temp, 0.1f);
+        transform.position = offset + Targetpos.position + new Vector3(0, higiht, 0);
+        transform.LookAt(Targetpos.position);
+    }
+}
